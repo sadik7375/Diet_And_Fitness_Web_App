e22540f38ca6fc52f343b72d374cde7859f855cc
sadik7375 <68754591+sadik7375@users.noreply.github.com>
2023-11-22 01:36:46 +0600

    mvc follow

---
+++ cliend_side_mvc/model/registerModel.php
@@ -0,0 +1,83 @@
+<?php 
+
+
+
+
+// $servername = "localhost"; // Replace with your server name
+// $username = "root"; // Replace with your database username
+// $password = ""; // Replace with your database password
+// $dbname = "fitness"; // Replace with your database name
+
+// // Create connection
+// $conn = new mysqli("localhost", "root", "", "fitness");
+
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+// echo "Connected successfully";
+
+// // Perform operations on the database here
+
+// // Close connection
+// $conn->close();
+
+
+
+
+    function registration($fullname,$email,$password)
+    {  
+        $conn = mysqli_connect("localhost","root","","fitness") or die("Connection failed : " . mysqli_connect_error());
+        // $conn = new mysqli("localhost", "root", "", "fitness");
+        $sql1 =  "INSERT INTO registration (fullname,email,password)
+                  VALUES ('{$fullname}','{$email}','{$password}')";
+        //   echo "<pre>";
+        //   print_r($sql1);
+        //   exit;
+    
+        $result = mysqli_query($conn,$sql1);
+        // $user= mysqli_fetch_assoc($result);
+        // echo $user;
+        // echo $result;
+
+      
+
+
+
+        if($result)
+        {
+            return true;
+        }
+        else
+        {
+             return false;
+            // echo "Error: " . mysqli_error($conn);
+        }
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
---
+++ cliend_side_mvc/view/table.php
@@ -0,0 +1,95 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>table</title>
+</head>
+<body>
+  <section class = "bg-gray-50 p-2 mt-12 mid-h-screen flex itmes-center justify-center" >
+    
+
+   <!-- container-->
+   <div class= "bg_cyan-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
+    
+
+<table class="w-full align-items: center">
+    <thead class="bg-gray-50 border-b-2 border-gray-200">
+      <tr>
+        <th class="w-20 p-3 text-sm font-semibold tracking-wide text left">Meal/Day of the week</th>
+        <th class="w-20 p-3 text-sm font-semibold tracking-wide text left">Saturday</th>
+        <th class="w-20 p-3 text-sm font-semibold tracking-wide text left">Sunday</th>
+        <th class="w-20 p-3 text-sm font-semibold tracking-wide text left">Monday</th>
+        <th class="w-20 p-3 text-sm font-semibold tracking-wide text left">Wednesday</th>
+        <th class="w-20 p-3 text-sm font-semibold tracking-wide text left">Thursday</th>
+        <th class="w-20 p-3 text-sm font-semibold tracking-wide text left">Friday</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="bg-gray-100">
+        <td class="p-3 text-sm text-gray-700">Breakfast</td>
+        <td class="p-3 text-sm text-gray-700">1 bowl oats and milk</td>
+        <td class="p-3 text-sm text-gray-700">bread and toast</td>
+        <td class="p-3 text-sm text-gray-700">chapati and veg</td>
+        <td class="p-3 text-sm text-gray-700">pankes with hony no suger</td>
+        <td class="p-3 text-sm text-gray-700">banana milk shake</td>
+        <td class="p-3 text-sm text-gray-700">cheat day</td>
+      </tr>
+      <tr class="bg-white-100">
+        <td class="p-3 text-sm text-gray-700">Snack</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+      </tr>
+      <tr class="bg-gray-100">
+        <td class="p-3 text-sm text-gray-700">Lunch</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+      </tr>
+      <tr class="bg-white-100">
+        <td class="p-3 text-sm text-gray-700">Snack</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+      </tr>
+      <tr class="bg-gray-100">
+        <td class="p-3 text-sm text-gray-700">Dinner</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+      </tr>
+      <tr class="bg-white-100">
+        <td class="p-3 text-sm text-gray-700">Optional Beverages</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+        <td class="p-3 text-sm text-gray-700">EMPTY</td>
+      </tr>
+    </tbody>
+  </table>
+   </div>
+ 
+  </section>
+ 
+</body>
+</html>
+
+
+
+  
